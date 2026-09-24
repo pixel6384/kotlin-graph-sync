8383a8eb8b5e05382b94aac78eecfcee4909d3bc
pixel6384 <helix7342@protonmail.com>
2026-09-24 16:45:21 +0700

    order sync operations to maintain graph integrity

--- src/main/kotlin/com/graphsync/GraphDiff.kt
+++ src/main/kotlin/com/graphsync/GraphDiff.kt
@@ -4,7 +4,7 @@ object GraphDiff {
     fun <T> calculateDiff(source: Graph<T>, target: Graph<T>): List<SyncOp<T>> {
         val ops = mutableListOf<SyncOp<T>>()
 
-        // 1. Nodes to add
+        // 1. Nodes to add (Must happen first so edges can reference them)
         source.nodes.filter { it !in target.nodes }.forEach {
             ops.add(SyncOp.AddNode(it))
         }
@@ -14,7 +14,7 @@ object GraphDiff {
             ops.add(SyncOp.AddEdge(it.first, it.second))
         }
 
-        // 3. Edges to remove
+        // 3. Edges to remove (Must happen before removing nodes they connect to)
         target.edges.filter { it !in source.edges }.forEach {
             ops.add(SyncOp.RemoveEdge(it.first, it.second))
         }
